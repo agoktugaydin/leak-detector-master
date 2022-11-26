0628a0f8370070fadecd9f5c0fdd5eb78bf0c86e
agoktugaydin <goktugaydin36@gmail.com>
2022-11-26 19:45:51 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+
+// oled ve pio icin kutuphaneler dahil edildi
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Arduino.h>
+
+// i2c kutuphanesi icin gerekli
+#include <Wire.h>
+
+// reset pini kullanilmiyor fakat i2c kutuphanesi icin gerekli
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+// pin and constant value assignees
+const int analogIn = A0;
+double RawValue = 0;
+double Voltage = 0;
+double rawSum = 0;
+
+void displayValues(){
+
+rawSum = analogRead(analogIn);
+
+// ornekleme 
+for (int i = 0; i < 500; i++){         
+    rawSum += analogRead(analogIn);
+}
+
+}
+
+void setup() {
+
+}
+
+void loop() {
+
+}
